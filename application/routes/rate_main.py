83bab5434683a2607d5197422741355af31c2aa6
minasssh <oleksandra.vedm@gmail.com>
2024-06-28 19:02:24 +0300

    updated

--- application/routes/rate_main.py
+++ application/routes/rate_main.py
@@ -8,3 +8,15 @@ bp = Blueprint('rate', __name__)
 def show_rates():
     rates = rate_util.get_all()
     return render_template('rate/rate_detail.html', rates=rates)
+
+
+@bp.route('/basic<int:rate_id>')
+def show_rate1(rate_id):
+    rate = rate_util.get_rate_by_id(rate_id)  # Здесь должна быть функция для получения тарифа по его ID
+    return render_template('rate/rate_main_page.html', rate=rate)
+
+
+@bp.route('/notbasic')
+def show_rate2():
+    rate = rate_util.get_rate_by_id(2)  # Здесь должна быть функция для получения тарифа по его ID
+    return render_template('rate/rate_main_page.html', rate=rate)
