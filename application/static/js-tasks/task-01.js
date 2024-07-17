61fcaf98a412d3cf7bce26ea924afe1a5158a57a
Massonus <maksym.lithuanian@gmail.com>
2024-07-17 21:12:00 +0300

    refactor

--- application/static/js-tasks/task-01.js
+++ application/static/js-tasks/task-01.js
@@ -1,14 +1,18 @@
+// Messi Goals
 // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
+// Make negative
 // https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 function makeNegative(num) {
     return num > 0 ? num * -1 : num;
 }
 
+// Game Move
 // https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 function move(position, roll) {
     return position + (roll * 2)
 }
+
