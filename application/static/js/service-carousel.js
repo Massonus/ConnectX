0e2e9814f7126f8b31a942e45015c07a89dbefae
Massonus <maksym.lithuanian@gmail.com>
2024-07-19 23:22:51 +0300

    fixed service carousel

--- application/static/js/service-carousel.js
+++ application/static/js/service-carousel.js
@@ -1,22 +1,21 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    var carousel = document.getElementById('carousel');
-    var slides = document.getElementsByClassName('slide');
-    var amount = slides.length;
-    var moveOffset = document.getElementById('carousel-container').offsetWidth;
+    let carousel = document.getElementById('carousel');
+    let slides = document.getElementsByClassName('slide');
+    let moveOffset = document.getElementById('carousel-container').offsetWidth;
 
     function updateWidths() {
         if (window.innerWidth <= 768) {
             moveOffset = document.getElementById('carousel-container').offsetWidth;
-            for (var i = 0; i < amount; i++) {
+            for (let i = 0; i < slides.length; i++) {
                 slides[i].style.width = moveOffset + 'px';
             }
         } else {
             moveOffset = document.getElementById('carousel-container').offsetWidth / 3;
-            for (var i = 0; i < amount; i++) {
+            for (let i = 0; i < slides.length; i++) {
                 slides[i].style.width = moveOffset + 'px';
             }
         }
-        carousel.style.width = (amount * moveOffset) + 'px';
+        carousel.style.width = (slides.length * moveOffset) + 'px';
     }
 
     window.addEventListener('resize', updateWidths);
@@ -27,22 +26,28 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById('next').addEventListener('click', next, true);
 
     function prev() {
-        carousel.insertBefore(carousel.children[amount - 1], carousel.children[0]);
-        carousel.style.transition = 'none';
-        carousel.style.transform = 'translateX(-' + moveOffset + 'px)';
-        setTimeout(function () {
-            carousel.style.transition = 'transform 0.5s';
-            carousel.style.transform = 'translateX(0)';
-        }, 20);
+        if (carousel.children.length > 0) {
+            carousel.insertBefore(carousel.children[carousel.children.length - 1], carousel.children[0]);
+            carousel.style.transition = 'none';
+            carousel.style.transform = 'translateX(-' + moveOffset + 'px)';
+            setTimeout(function () {
+                carousel.style.transition = 'transform 0.5s';
+                carousel.style.transform = 'translateX(0)';
+            }, 20);
+        }
     }
 
     function next() {
-        carousel.style.transition = 'transform 0.5s';
-        carousel.style.transform = 'translateX(-' + moveOffset + 'px)';
-        setTimeout(function () {
-            carousel.appendChild(carousel.children[0]);
-            carousel.style.transition = 'none';
-            carousel.style.transform = 'translateX(0)';
-        }, 500);
+        if (carousel.children.length > 0) {
+            carousel.style.transition = 'transform 0.5s';
+            carousel.style.transform = 'translateX(-' + moveOffset + 'px)';
+            setTimeout(function () {
+                if (carousel.children.length > 0) {
+                    carousel.appendChild(carousel.children[0]);
+                    carousel.style.transition = 'none';
+                    carousel.style.transform = 'translateX(0)';
+                }
+            }, 500);
+        }
     }
 });
