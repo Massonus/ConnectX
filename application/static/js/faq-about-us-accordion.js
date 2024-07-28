a75617aacafd3583f11105d8997b5ed0230ca007
Massonus <maksym.lithuanian@gmail.com>
2024-07-28 14:21:05 +0300

    refactor faq about us accordion

--- application/static/js/faq-about-us-accordion.js
+++ application/static/js/faq-about-us-accordion.js
@@ -1,22 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     const faqItems = document.querySelectorAll(".faq-item");
+    const FAQ_PADDING_CLOSED = "0";
+    const FAQ_PADDING_OPEN = "1em 0";
 
     faqItems.forEach((item) => {
-        item.querySelector(".faq-question").addEventListener("click", () => {
+        const question = item.querySelector(".faq-question");
+
+        question.addEventListener("click", () => {
             const openItem = item.parentElement.querySelector(".faq-item-open");
 
             if (openItem && openItem !== item) {
                 openItem.classList.remove("faq-item-open");
                 const openAnswer = openItem.querySelector(".faq-answer");
-                openAnswer.style.padding = "0";
+                openAnswer.style.padding = FAQ_PADDING_CLOSED;
             }
 
             item.classList.toggle("faq-item-open");
             const answer = item.querySelector(".faq-answer");
             if (item.classList.contains("faq-item-open")) {
-                answer.style.padding = "1em 0";
+                answer.style.padding = FAQ_PADDING_OPEN;
             } else {
-                answer.style.padding = "0";
+                answer.style.padding = FAQ_PADDING_CLOSED;
             }
         });
     });
