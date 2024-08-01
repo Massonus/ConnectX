da966f7e59ce10f9b2bf837a016f28dbba2100d5
Massonus <maksym.lithuanian@gmail.com>
2024-08-01 15:05:22 +0300

    refactor faq about us logic

--- application/static/js/faq-about-us-accordion.js
+++ application/static/js/faq-about-us-accordion.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const faqItems = document.querySelectorAll(".faq-item");
-    const faqClosed = "0";
-    const faqOpen = "1em 0";
 
     faqItems.forEach((item) => {
         const question = item.querySelector(".faq-question");
@@ -11,17 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (openItem && openItem !== item) {
                 openItem.classList.remove("faq-item-open");
-                const openAnswer = openItem.querySelector(".faq-answer");
-                openAnswer.style.padding = faqClosed;
             }
 
             item.classList.toggle("faq-item-open");
-            const answer = item.querySelector(".faq-answer");
-            if (item.classList.contains("faq-item-open")) {
-                answer.style.padding = faqOpen;
-            } else {
-                answer.style.padding = faqClosed;
-            }
         });
     });
 });
