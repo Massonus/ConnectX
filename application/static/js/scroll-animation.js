6dfe476f12debdf05961b9149a44221eb479d406
Danylo Voloshyn <danilovoloshyn@gmail.com>
2024-08-01 12:02:40 +0300

    Fixed the style and animation for the "We connect you" block

--- application/static/js/scroll-animation.js
+++ application/static/js/scroll-animation.js
@@ -1,27 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
-      const leftElements = document.querySelectorAll('.fade-in-left');
-      const rightElements = document.querySelectorAll('.fade-in-right');
-  
-      const observer = new IntersectionObserver(entries => {
-          entries.forEach(entry => {
-              if (entry.isIntersecting) {
-                  if (entry.target.classList.contains('fade-in-left')) {
-                      entry.target.classList.add('animate-left');
-                  } else if (entry.target.classList.contains('fade-in-right')) {
-                      entry.target.classList.add('animate-right');
-                  }
-              }
-          });
-      }, {
-          threshold: 0.5
-      });
-  
-      leftElements.forEach(element => {
-          observer.observe(element);
-      });
-  
-      rightElements.forEach(element => {
-          observer.observe(element);
-      });
-  });
-  
+    const leftElements = document.querySelectorAll('.fade-in-left');
+    const rightElements = document.querySelectorAll('.fade-in-right');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                console.log('Element is intersecting:', entry.target);
+                if (entry.target.classList.contains('fade-in-left')) {
+                    entry.target.classList.add('animate-left');
+                } else if (entry.target.classList.contains('fade-in-right')) {
+                    entry.target.classList.add('animate-right');
+                }
+                observer.unobserve(entry.target);
+            } else {
+                console.log('Element is not intersecting:', entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1,
+        rootMargin: '0px 0px -10% 0px'
+    });
+
+    leftElements.forEach(element => {
+        observer.observe(element);
+    });
+
+    rightElements.forEach(element => {
+        observer.observe(element);
+    });
+
+    document.documentElement.style.overflowX = 'hidden';
+});
