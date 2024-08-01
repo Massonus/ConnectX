f5a6bdd5bd16293176c296dc4ac40b97a0c28cba
Massonus <maksym.lithuanian@gmail.com>
2024-08-01 15:34:07 +0300

    updated faq about us accordion logic

--- application/static/js/faq-about-us-accordion.js
+++ application/static/js/faq-about-us-accordion.js
@@ -9,9 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (openItem && openItem !== item) {
                 openItem.classList.remove("faq-item-open");
+                const openAnswer = openItem.querySelector(".faq-answer");
+                openAnswer.classList.remove('accordion-content-open');
+                openAnswer.classList.add('accordion-content-closed');
+                openAnswer.style.maxHeight = null;
             }
 
-            item.classList.toggle("faq-item-open");
+            const answer = item.querySelector(".faq-answer");
+            const isOpen = answer.style.maxHeight;
+
+            if (isOpen) {
+                answer.classList.remove('accordion-content-open');
+                answer.classList.add('accordion-content-closed');
+                answer.style.maxHeight = null;
+            } else {
+                answer.classList.add('accordion-content-open');
+                answer.classList.remove('accordion-content-closed');
+                answer.style.maxHeight = answer.scrollHeight + "px";
+            }
+
+            item.classList.toggle("faq-item-open", !isOpen);
         });
     });
 });
